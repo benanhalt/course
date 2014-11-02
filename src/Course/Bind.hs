cb4e49609ae4b10a8b83a2c80376df770e684042
Ben Anhalt <ben.anhalt@gmail.com>
2014-11-01 23:32:59 -0500

    finished bind.

--- src/Course/Bind.hs
+++ src/Course/Bind.hs
@@ -63,8 +63,7 @@ infixr 1 =<<
   f (a -> b)
   -> f a
   -> f b
-(<*>) =
-  error "todo"
+f <*> a = (<$> a) =<< f
 
 infixl 4 <*>
 
@@ -77,8 +76,8 @@ instance Bind Id where
     (a -> Id b)
     -> Id a
     -> Id b
-  (=<<) =
-    error "todo"
+  f =<< (Id a) = f a
+
 
 -- | Binds a function on a List.
 --
@@ -89,8 +88,9 @@ instance Bind List where
     (a -> List b)
     -> List a
     -> List b
-  (=<<) =
-    error "todo"
+  f =<< Nil = Nil
+  f =<< (a :. as) = (f a) ++ (f =<< as)
+
 
 -- | Binds a function on an Optional.
 --
@@ -101,8 +101,8 @@ instance Bind Optional where
     (a -> Optional b)
     -> Optional a
     -> Optional b
-  (=<<) =
-    error "todo"
+  f =<< Empty = Empty
+  f =<< (Full a) = f a
 
 -- | Binds a function on the reader ((->) t).
 --
@@ -113,8 +113,7 @@ instance Bind ((->) t) where
     (a -> ((->) t b))
     -> ((->) t a)
     -> ((->) t b)
-  (=<<) =
-    error "todo"
+  (f =<< g) t = f (g t) t
 
 -- | Flattens a combined structure to a single structure.
 --
@@ -133,8 +132,7 @@ join ::
   Bind f =>
   f (f a)
   -> f a
-join =
-  error "todo"
+join = (id =<<)
 
 -- | Implement a flipped version of @(=<<)@, however, use only
 -- @join@ and @(<$>)@.
@@ -147,8 +145,7 @@ join =
   f a
   -> (a -> f b)
   -> f b
-(>>=) =
-  error "todo"
+(>>=) a f = join (f <$> a)
 
 infixl 1 >>=
 
@@ -163,8 +160,8 @@ infixl 1 >>=
   -> (a -> f b)
   -> a
   -> f c
-(<=<) =
-  error "todo"
+(<=<) f g a = f =<< (g a)
+
 
 infixr 1 <=<
 
